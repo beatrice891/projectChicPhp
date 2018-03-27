92b4afed951ce6c7df2d4adcf515c33909ba8a6a
Beatrice <csillig.beatrice@gmail.com>
2018-03-27 23:13:11 +0300

    create file category.php(select all item in db belongs to catID)

--- category.php
+++ category.php
@@ -1,1 +1,14 @@
-<p>Category</p>
+
+<?php
+//if categoryID is not press,redirect to index.php
+	if(!isset($_GET['categoryID'])){
+		header('Location:index.php');
+	}
+//Select all items that belongs to selected categoryID
+	//Create a join query
+	$stock_sql = "SELECT stock.stockID, stock.name, stock.price, category.name AS catname FROM stock JOIN category ON stock.categoryID=category.categoryID WHERE stock.categoryID=".$_GET['categoryID'];
+	if($stock_query = mysqli_query($dbconnect, $stock_sql)){
+		$stock_rs = mysqli_fetch_assoc($stock_query);
+	}
+
+?>
