38b8b0c592156560ddc6c619211bd4447fd617d5
Beatrice <csillig.beatrice@gmail.com>
2018-03-27 23:33:29 +0300

    displaying and formatting dynamic query

--- category.php
+++ category.php
@@ -10,5 +10,22 @@
 	if($stock_query = mysqli_query($dbconnect, $stock_sql)){
 		$stock_rs = mysqli_fetch_assoc($stock_query);
 	}
+	//if is no item in the stock
+	if(mysqli_num_rows($stock_query) == 0){
+		echo "Sorry we dont have that item in stock";
+	}else{ ?>
+			<h1><?php echo $stock_rs['catname'];?></h1>
+			<?php
+				do{ ?>
+				<div class="item">
+					<p><?php echo $stock_rs['name'];?></p>
+					<p>$<?php echo $stock_rs['price'];?></p>
+				</div>
+
+				<?php }while($stock_rs = mysqli_fetch_assoc($stock_query));
+
+
+			?>
+	<?php }
 
 ?>
