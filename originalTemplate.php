188f6261e5b38d73fefeccd007cd7a89350f4f9e
Beatrice <csillig.beatrice@gmail.com>
2018-03-27 13:52:46 +0300

    added the originalTemplate.php

---
+++ originalTemplate.php
@@ -0,0 +1,53 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Chic magazine</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="container">
+		<div class="header">
+			<div class="logo">
+				<a href="index.php"><img src="images/logo.jpg" alt="Chic logo"/></a>
+			</div>
+		<div class="navigation">
+			<p>Links go here
+				<a href="admin.php"></a>
+			</p>
+		</div>
+		</div>
+		<div class="mainContent">
+		<p>text</p>
+		</div>
+		<div class="secondContent">
+			<table width="259" border="0" cellspacing="0" cellpadding="0">
+				<tr>
+					<td width="80" align="top" class="textheading">Where:</td>
+					<td width="179">Merivale Mall</td>
+					<p>Something</p>
+				</tr>
+				<tr>
+					<td>&nbsp;</td>
+					<td>&nbsp;</td>
+				</tr>
+				<tr>
+					<td align="top" class="textheading">When:</td>
+					<td><p>Luni - Vineri : 09:00 - 18:00</p><p>Sambata - Duminica : 10:00 - 17:00</p></td>
+				</tr>
+				<tr>
+					<td>&nbsp;</td>
+					<td>&nbsp;</td>
+				</tr>
+				<tr>
+					<td>&nbsp;</td>
+					<td>&nbsp;</td>
+				</tr>
+			</table>
+		</div>
+		<div class="footer">
+		</div>
+	</div>
+</body>
+</html>
