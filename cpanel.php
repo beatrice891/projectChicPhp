c506a96571e82ec40b8d389b37603e1364e35431
Beatrice <csillig.beatrice@gmail.com>
2018-03-28 22:59:06 +0300

    create url for add category

--- cpanel.php
+++ cpanel.php
@@ -5,5 +5,6 @@ action=logout - will see through to the admin page
 -->
 <h1>Control Panel</h1>
 <p><a href="index.php?page=admin&action=logout">Logout</a></p>
+<p><a href="index.php?page=addcategory">Add new category</a></p>
 <?php
 ?>
