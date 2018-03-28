282d280a3f140f831fec928e5be229a2edfaee57
Beatrice <csillig.beatrice@gmail.com>
2018-03-28 23:23:23 +0300

    create the form to add new category

--- addcategory.php
+++ addcategory.php
@@ -5,6 +5,8 @@
 		header('Location:index.php?page=admin');
 	}
 ?>
-
-
 <h1>Add new category</h1>
+<form method="post" action="index.php?page=entercategory">
+	<p><input type="text" name="name" size="40" maxlength="50"></p>
+	<p><input type="submit" name="submit" value="Add category"></p>
+</form>
