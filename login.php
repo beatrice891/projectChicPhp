840c6e9b4638185f2473aa847aaa58e64ae0a65a
Beatrice <csillig.beatrice@gmail.com>
2018-03-28 22:33:28 +0300

    create the form and the cond for error

--- login.php
+++ login.php
@@ -1,4 +1,14 @@
 <h1>Login</h1>
-<?php
+<form name="login" method="post" action="index.php?page=admin">
+	<p>Username <input name="username" type="text" maxlength=30/></p>
+	<p>Password <input name="password" type="password" maxlength=30/></p>
+	<!--if someone try to login but the session is not set then display this error msg -->
+	<?php
+		if(isset($_POST['login']) && !isset($_SESSION['admin'])){
+			?>
+			<p><span class="error">Incorect username or password</span></p>
+		<?php }
 
-?>
+	 ?>
+	<p><input type="submit" name="login" value="Submit"/></p>
+</form>
