0d44f787ee9b03eaa535cbc1f19cf825626e4e28
Beatrice <csillig.beatrice@gmail.com>
2018-03-29 22:46:17 +0300

    replace $_POST with $_SESSION, and unset the $_SESSION

--- entercategory.php
+++ entercategory.php
@@ -5,14 +5,18 @@
 		header('Location:index.php?page=admin');
 	}
 //check to see if user has submitted the add category form
-	// if(!isset($_POST['submit'])){
-	// 	header('Location:index.php?page=admin');
-	// }
+	if(!isset($_SESSION['addcategory']['name'])){
+		header('Location:index.php?page=admin');
+	}
 	//remove the confirmation submit button and added to confirmcategory.php
+
 //enter the new category
-	$name = mysqli_real_escape_string($dbconnect, $_POST['name']);
+	$name = mysqli_real_escape_string($dbconnect, $_SESSION['addcategory']['name']);
 	$newcategory_sql = "INSERT INTO category (name) VALUES('$name')";
 	$newcategory_query = mysqli_query($dbconnect, $newcategory_sql);
+
+//remove the data has inserted in input
+	unset($_SESSION['addcategory']['name']);
 ?>
 <p>New category has been entered</p>
 <p><a href="index.php?page=admin">Return to admin panel</a></p>
