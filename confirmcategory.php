3f64c230a8ce889488849038a08dcd7cf934a849
Beatrice <csillig.beatrice@gmail.com>
2018-03-29 21:59:05 +0300

    set a new session and add the link to go back or confirm

---
+++ confirmcategory.php
@@ -0,0 +1,17 @@
+<?php
+//
+session_start();
+//check to see if the user is actually logged in, if not redirect
+	if(!isset($_SESSION['admin'])){
+		header('Location:index.php?page=admin');
+	}
+//check to see if the user submitted the add category form
+	if(isset($_POST['submit'])){
+		header('Location:index.php?page=admin');
+	}
+// set a new session category
+	$_SESSION['addcategory']['name'] = $_POST['name'];
+?>
+<h1>Confirm category name</h1>
+<p>You enter:<?php echo $_POST['name'];?></p>
+<p><a href="index.php?page=entercategory">Correct, continu</a> | <a href="index.php?page=addcategory">Go back</a></p>
