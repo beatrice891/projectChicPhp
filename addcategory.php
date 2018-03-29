048f38937bc78fea435f237fbd91b38ab71a327f
Beatrice <csillig.beatrice@gmail.com>
2018-03-29 21:58:07 +0300

    add value with $_SESSION

--- addcategory.php
+++ addcategory.php
@@ -7,6 +7,6 @@
 ?>
 <h1>Add new category</h1>
 <form method="post" action="index.php?page=confirmcategory"><!--modify the entercategory with confirmcategory-->
-	<p><input type="text" name="name" size="40" maxlength="50"></p>
+	<p><input type="text" name="name" size="40" value="<?php echo $_SESSION['addcategory']['name'];?>" maxlength="50"></p>
 	<p><input type="submit" name="submit" value="Add category"></p>
 </form>
