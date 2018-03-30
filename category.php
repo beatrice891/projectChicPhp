8ce95d1058cb48878557c79d3ea29dbf4679e2d5
Beatrice <csillig.beatrice@gmail.com>
2018-03-30 21:25:40 +0300

    add join and images

--- category.php
+++ category.php
@@ -5,7 +5,7 @@
 	}
 //Select all items that belongs to selected categoryID
 	//Create a join query
-	$stock_sql = "SELECT stock.stockID, stock.name, stock.price, category.name AS catname FROM stock JOIN category ON stock.categoryID=category.categoryID WHERE stock.categoryID=".$_GET['categoryID'];
+	$stock_sql = "SELECT stock.stockID, stock.name, stock.price, stock.thumbnail, category.name AS catname FROM stock JOIN category ON stock.categoryID=category.categoryID WHERE stock.categoryID=".$_GET['categoryID'];
 	if($stock_query = mysqli_query($dbconnect, $stock_sql)){
 		$stock_rs = mysqli_fetch_assoc($stock_query);
 	}
@@ -17,8 +17,10 @@
 			<?php
 				do{ ?>
 				<div class="item">
-					<a href="index.php?page=item&stockID=<?php echo $stock_rs['stockID'];?>"><p><?php echo $stock_rs['name'];?></p>
-					<p><?php echo $stock_rs['price'];?></p></a>
+					<a href="index.php?page=item&stockID=<?php echo $stock_rs['stockID'];?>">
+					<p><img src="images/<?php echo $stock_rs['thumbnail'];?>"</p>	
+					<p><?php echo $stock_rs['name'];?></p>
+					<p>Price: $<?php echo $stock_rs['price'];?></p></a>
 				</div>
 
 				<?php }while($stock_rs = mysqli_fetch_assoc($stock_query));
